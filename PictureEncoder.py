de0c959fc0864bca150140003dc4400bef4ab95a
tevon <tevoneyabi@gmail.com>
2023-03-02 17:11:23 +0100

    conversion to bitmap registers

--- PictureEncoder.py
+++ PictureEncoder.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 
 
-def encode(source, amplitudeResolution, angularResolution, isCircular):
-    """ Takes an image and returns the Polar 
-    representation with the preffered 
+def encode(source, amplitudeResolution, angularResolution, isCircular, bitmap=False):
+    """ Takes an image and returns the Polar
+    representation with the preffered
     amplitude and angular dimensions
     Args:
         source (np.array): Image
@@ -18,7 +18,10 @@ def encode(source, amplitudeResolution, angularResolution, isCircular):
     polar_image = transform2Poles(source, isCircular)
     encodedImage = setResolution(
         polar_image, amplitudeResolution, angularResolution)
-    return encodedImage
+
+    if not bitmap:
+        return encodedImage
+    return toBitmap(encodedImage)
 
 
 def transform2Poles(source, isCircular):
@@ -60,3 +63,38 @@ def setResolution(image, amplitudeResolution, AngularResolution):
     newDim = (amplitudeResolution, AngularResolution)
     resizedImg = cv2.resize(image, newDim, interpolation=cv2.INTER_AREA)
     return resizedImg
+
+
+def toBitmap(image, bigEndian=True, serial=2):
+    if(image.shape[1] % serial != 0):
+        print(f"image shape {image.shape} is not serializable with {serial}")
+        return
+    bits = np.array([[[list('{0:08b}'.format(num))
+                    for num in color] for color in row] for row in image])
+    # Convert the bits to integers and reshape to 3D
+    result = bits.astype(int).reshape(
+        (image.shape[0], image.shape[1], image.shape[2], -1))
+    print(result.shape)
+    bitmap = []
+    order = [1, 0, 2]
+    for i, row in enumerate(result):
+        if (i < len(result)-1):
+            continue
+        registers = []
+        for j in range(result.shape[1]):
+            print(f"{row[j][0]} {row[j][1]} {row[j][2]}")
+
+        for k in order:
+            arr = np.arange(result.shape[1]//serial)
+            if bigEndian:
+                arr = np.arange(result.shape[1]//serial-1, -1, -1)
+            for s in range(serial):
+                [registers.append(np.dot(row[s::serial, k, x], 2 ** arr))
+                    for x in range(7)]
+        bitmap.append(np.array(registers))
+        print(np.array(registers))
+    return bitmap
+
+
+source = cv2.imread("kul.png")
+encode(source, 6, 3, True, True)
--- demoEncoder.py
+++ demoEncoder.py
@@ -9,10 +9,13 @@ from time import sleep
 # cv2.imshow("Encoded Image", encodedImage)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-maxAmp = 40
-angleSlices = 300
-im = cv2.imread("earth-icon.jpg")
-polar = PictureEncoder.encode(im, maxAmp, angleSlices, True)
+maxAmp = 50
+angleSlices = 200
+ledSize = 1
+offset = 1
+im = cv2.imread("thomas more.png")
+polar = PictureEncoder.encode(im, maxAmp, angleSlices, False)
+cv2.imwrite("polar.jpg", polar)
 print(polar)
 # cv2.imshow("Encoded Image", polar)
 # cv2.waitKey(0)
@@ -30,7 +33,7 @@ def resize(img, scale=1):
     return cv2.resize(img, (dim, dim), interpolation=cv2.INTER_AREA)
 
 
-scale = 35
+scale = 20
 dim = (maxAmp*scale, maxAmp*scale, 3)
 canvas = np.zeros(dim)
 center = canvas.shape[0]//2
@@ -41,19 +44,19 @@ print(f"amp: {maxAmp}")
 print(f"ang: {angleSlices}")
 while True:
     angle = int(np.round(phiScale*phi))
-    for i in range(0, maxAmp):
+    for i in range(1+offset, maxAmp):
         x, y = pol2cart((center/maxAmp)*i, phi)
 
         color = (
             polar[int(min(np.round(phiScale*phi), angleSlices-1)), i]/255).tolist()
         print(f"ang: {angle}, amp: {i}, color: {color}")
         canvas = cv2.circle(canvas, (x+center, y+center),
-                            3, color, -1)
-    cv2.imshow("Original", resize(canvas))
+                            ledSize, color, -1)
+    cv2.imshow("Original", resize(canvas, 1))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    phi += 0.01
+    phi += 0.02
     if phi >= np.pi*2-0.0001:
         phi = 0
         canvas = np.zeros(dim)
-    sleep(0.001)
+    # sleep(0.001)
